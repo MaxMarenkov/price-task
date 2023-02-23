1a10b67765f0fbcbc140c080b1427687eb22b4e8
Maxim Marenkov <max.marenkov@gmail.com>
2023-02-23 17:39:54 +0400

    PriceController & tests added

--- src/Controller/PriceController.php
+++ src/Controller/PriceController.php
@@ -4,17 +4,38 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Request\PriceRequest;
+use App\Request\RequestConverter;
+use App\Service\PriceUpdater;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PriceController extends AbstractController
 {
+    private RequestConverter $converter;
+    private PriceUpdater $priceUpdater;
+
+    public function __construct(RequestConverter $converter, PriceUpdater $priceUpdater)
+    {
+        $this->converter = $converter;
+        $this->priceUpdater = $priceUpdater;
+    }
+
     #[Route('/prices', name: 'prices', methods: ['POST'])]
     public function index(Request $request): JsonResponse
     {
-        return $this->json([], Response::HTTP_OK);
+        try {
+            /** @var PriceRequest $priceRequest */
+            $priceRequest = $this->converter->convertRequest($request, PriceRequest::class);
+        } catch (BadRequestHttpException $e) {
+            return new JsonResponse(status: Response::HTTP_BAD_REQUEST);
+        }
+        $this->priceUpdater->processPriceRequest($priceRequest);
+
+        return $this->json(['price' => $priceRequest->getPrice()], Response::HTTP_OK);
     }
 }
--- tests/Functional/PriceControllerTest.php
+++ tests/Functional/PriceControllerTest.php
@@ -16,6 +16,8 @@ use Symfony\Component\HttpFoundation\Response;
 class PriceControllerTest extends WebTestCase
 {
     private KernelBrowser $client;
+    private ?EntityManagerInterface $em;
+    private ?PriceRepository $priceRepository;
 
     protected function setUp(): void
     {
@@ -23,12 +25,122 @@ class PriceControllerTest extends WebTestCase
         $this->client = static::createClient();
         $this->client->disableReboot();
 
+        $this->em = static::getContainer()->get(EntityManagerInterface::class);
+        $this->em->getConnection()->beginTransaction();
+        $this->priceRepository = $this->em->getRepository(Price::class);
         parent::setUp();
     }
 
-     public function testController(): void
+    protected function tearDown(): void
+    {
+        $this->em->getConnection()->rollBack();
+        parent::tearDown();
+    }
+
+    public function negativeDataProvider(): Generator
+    {
+        yield 'empty request' => [[]];
+
+        yield 'invalid currency' => [[
+            'price' => 10000,
+            'currency' => 'xxx',
+            'variant' => '42',
+            'product' => 'Adidas Shoes',
+            'category' => 'Shoes',
+        ]];
+
+        yield 'extra field' => [[
+            'price' => 10000,
+            'currency' => 'USD',
+            'variant' => '42',
+            'product' => 'Adidas Shoes',
+            'category' => 'Shoes',
+            'extra' => 'extra',
+        ]];
+
+        yield 'missing field' => [[
+            'price' => 10000,
+            'product' => 'Adidas Shoes',
+            'category' => 'Shoes',
+        ]];
+    }
+
+     /**
+      * @dataProvider negativeDataProvider
+      */
+     public function testNegativeScenarios(array $data): void
      {
-         $this->client->request(Request::METHOD_POST, '/prices');
-         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+         $this->client->request(Request::METHOD_POST, '/prices', $data);
+         $this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
      }
+
+    public function testUnifiedCategory(): void
+    {
+        $this->client->jsonRequest(Request::METHOD_POST, '/prices', [
+            'price' => $price = 12000,
+            'currency' => $currency = 'USD',
+            'variant' => $variant = '42',
+            'product' => $product = 'Adidas',
+            'category' => 'Shoes',
+        ]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        /** @var Price[] $entities */
+        $entities = $this->priceRepository->findBy([
+            'currency' => $currency,
+            'variant' => $variant,
+            'product' => $product,
+        ]);
+
+        $this->assertCount(1, $entities);
+        $this->assertEquals($price, array_pop($entities)->getPrice());
+
+        $this->client->jsonRequest(Request::METHOD_POST, '/prices', [
+            'price' => $price = 9900,
+            'currency' => $currency,
+            'variant' => '40',
+            'product' => $product,
+            'category' => 'Shoes',
+        ]);
+
+        /** @var Price[] $entities */
+        $entities = $this->priceRepository->findBy([
+            'currency' => $currency,
+            'product' => $product,
+        ]);
+
+        $this->assertCount(2, $entities);
+        $this->assertEquals($price, array_pop($entities)->getPrice());
+        $this->assertEquals($price, array_pop($entities)->getPrice());
+    }
+
+    public function testNonUnifiedCategory(): void
+    {
+        $this->client->jsonRequest(Request::METHOD_POST, '/prices', [
+            'price' => $priceA = 12000,
+            'currency' => $currency = 'EUR',
+            'variant' => 'long',
+            'product' => $product = 'Jewelry product',
+            'category' => 'Jewelry',
+        ]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->client->jsonRequest(Request::METHOD_POST, '/prices', [
+            'price' => $priceB = 9900,
+            'currency' => $currency,
+            'variant' => 'short',
+            'product' => $product,
+            'category' => 'Jewelry',
+        ]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        /** @var Price[] $entities */
+        $entities = $this->priceRepository->findBy([
+            'currency' => $currency,
+            'product' => $product,
+        ]);
+
+        $this->assertCount(2, $entities);
+        $this->assertEquals($priceB, array_pop($entities)->getPrice());
+        $this->assertEquals($priceA, array_pop($entities)->getPrice());
+    }
 }
